997f85d91e4aa701dc6cd8360ab6cc21576d3d94
DenkyOgbonnaya <denkyogb@gmail.com>
2019-05-07 15:04:39 +0100

    added dropdown button to header navbar

--- client/src/components/navs/navBar.js
+++ client/src/components/navs/navBar.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import {useGlobal} from 'reactn';
 import {
-  Container, Row, Col, Input, NavLink, NavbarBrand, Button, Navbar} from 'reactstrap';
-  import {NavLink as RRNavLink, Link} from 'react-router-dom';
+  Container, Row, Col, Input, NavLink, NavbarBrand, Button, Navbar,
+Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav,NavItem} from 'reactstrap';
+  import {NavLink as RRNavLink, Link, withRouter} from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = props => {
 const[searchedMeal, setSearchedMeal] = useGlobal('searchedMeal');
 const[cart] = useGlobal('cart');
 const[user, setUser] = useGlobal('currentUser');
-const[showSideNav, setShowSideNav] = useGlobal('showSideNav')
+const[showSideNav, setShowSideNav] = useGlobal('showSideNav');
+const[dropdownOpen, setDropDownOpen] = useState(false);
 
 const handleSearchChange = e => {
   setSearchedMeal(e.target.value)
@@ -22,13 +24,30 @@ const logoutUser = () => {
       
         <Container> 
           <Row> 
-          <Col xs = '6' md='10'> </Col>
-            <Col xs = '6' md='2'> { user ? <div className= 'right'><Link to = '/orders' className = 'link'>{user.userName}</Link>  <Link to = '/' onClick = {() => logoutUser()} className = 'link'>Logout</Link></div> : 
-            <div className= 'right' ><Link to = '/login' className = 'link'>Login</Link>  <Link to = '/signup' className = 'link'>sign Up</Link></div>} </Col>
+            <Col xs = '6' md='10'> </Col>
+            <Col xs = '6' md='2'> 
+            { user ?
+              <Dropdown nav isOpen={dropdownOpen} toggle={() => setDropDownOpen(!dropdownOpen)}className= 'right' >
+            <DropdownToggle caret style = {{background: '#8bc34a', color: '#424242'}}>
+              {user.userName}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header> Dashboard  </DropdownItem>
+              <DropdownItem onClick = {() => props.history.push('/profile')}> Profile </DropdownItem>
+              <DropdownItem onClick = {() => props.history.push('/orders')}>Orders</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem onClick = {() => logoutUser()} > Logout </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+             :
+            <div className= 'right' ><Link to = '/login' className = 'link'>Login</Link>  <Link to = '/signup' className = 'link'>sign Up</Link></div>
+          
+            } 
+             </Col>
             <Col xs = '12' md='12'>
               <Row>
               <Col xs ='2' md= '1'> <img src='images/icons/menu_ic.png' alt='menu' onClick= {() => setShowSideNav(!showSideNav)} className='menu'/>  </Col>
-                <Col xs ='6' md='9'> <NavbarBrand > FastMeals </NavbarBrand> </Col>
+                <Col xs ='6' md='9'> <NavbarBrand style = {{fontSize:'25px'}}> FastMeals </NavbarBrand> </Col>
                 <Col xs = '4' md='2'> <div className = 'right'><NavLink to= '/cart' tag={RRNavLink}> <img src='images/icons/shopping_cart.png' alt='cart' />
                 <span style={{color:'white'}}  >{cart.length} </span>
                 </NavLink> </div> </Col>
@@ -41,4 +60,4 @@ const logoutUser = () => {
     );
   
 }
-export default NavBar;
+export default withRouter(NavBar);
