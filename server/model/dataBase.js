b977a0b4e0b48c6b11a3f026f9e28b80048d31d6
DenkyOgbonnaya <denkyogb@gmail.com>
2019-05-23 02:40:01 +0100

    connected to mlab db

--- server/model/dataBase.js
+++ server/model/dataBase.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const dbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/fastmeals';
+const{MONGODB_URL} = process.env;
+
+const dbUrl = process.env.MONGO_URL || MONGODB_URL //'mongodb://localhost:27017/fastmeals';
 
 const connectToDb = () => {
     mongoose.connect(dbUrl, {useNewUrlParser: true})
